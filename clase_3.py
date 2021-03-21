edc6ddee5f6023705812a43e406c82656337c213
Robinson Ordoñez <robinsonor4@gmail.com>
2021-03-21 15:24:23 -0500

    ejercicios vectores, funciones y parametros

---
+++ clase_3.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 21 12:30:20 2021
+
+@author: DIOS BENDITO
+"""
+# HUF que dado un conjunto de numeros ingresados por el usuario
+# indique cuantos de estos son pares, empleando vectores
+
